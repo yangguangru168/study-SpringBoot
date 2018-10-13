0f183460d91697972b1594cedba95ab062114cc5
yangguangru168 <yanggunargu6>
2018-10-13 20:44:07 +0800

    Revert "再次测试提交"
    This reverts commit b0d0c01ae24de2ec035de1b040123ac0172acf24.

--- src/main/java/com/hand/girl/girlenum/ResultEnum.java
+++ src/main/java/com/hand/girl/girlenum/ResultEnum.java
@@ -6,7 +6,7 @@ public enum  ResultEnum {
     UNKNOWN(-1,"系统异常"),
     SUCCESS(1,"成功了"), 
     M_SCHOOL(100,"还在上小学"),
-    H_SCHOOL(10,"还在上中学aa");
+    H_SCHOOL(10,"还在上中学");
     private Integer code;
     private String msg;
     ResultEnum(Integer code, String msg) {
